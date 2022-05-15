3880c4eeaf2b96b98932af2aca0f3f5500ee9676
Evan W <ewitulsk@calpoly.edu>
2022-05-15 15:29:40 -0700

    structure

---
+++ src/header/Header.js
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <div class="grid grid-cols-5 gap 4">
+      <div>
+        <div>LOGO</div>
+      </div>
+      <div>
+        <div>Swap</div>
+      </div>
+      <div>
+        <div>Pool</div>
+      </div>
+      <div>
+        <div>Select Chain</div>
+      </div>
+      <div>
+        <div>Connect Wallet</div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- src/homePage/App.js
+++ src/homePage/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import Header from "../header/Header";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header/>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to our Nested AMM
+      </h1>
+    </div>
   )
 }
 
