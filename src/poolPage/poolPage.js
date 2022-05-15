fe37fd18306a39ba2ca9e22f675a3bdcc5fa990b
Evan W <ewitulsk@calpoly.edu>
2022-05-15 15:41:50 -0700

    pool/swap structure

---
+++ src/poolPage/poolPage.js
@@ -0,0 +1,14 @@
+import Header from "../header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1 className="text-3xl">
+        Pool Page
+      </h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/swapPage/swapPage.js
@@ -0,0 +1,14 @@
+import Header from "../header/Header";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1 className="text-3xl">
+        Swap Page
+      </h1>
+    </div>
+  )
+}
+
+export default App;
