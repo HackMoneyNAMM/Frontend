e4966e05c23487a81e8dcc55b872e9aa2743792e
Trevor Bryant <tbryant.tbryant@gmail.com>
2022-05-15 19:01:26 -0700

    Mock pool creation added

--- src/PoolPage/PoolPage.js
+++ src/PoolPage/PoolPage.js
@@ -1,12 +1,45 @@
+import { useState } from "react";
 import Header from "../header/Header";
 
+function PoolList(props) {
+  const poolList = props.toBeListed.map((pool) => 
+  <li key={pool.name}>
+    {pool.name}
+  </li>
+  );
+
+  return (
+    <ul>{poolList}</ul>
+  );
+}
+
 function PoolPage() {
+
+  const [pools, setPools] = useState([])
+
+  async function makePool() {
+    console.log("Making Pool");
+    const newPool = {
+      name: "TestPool",
+      tokens: []
+    };
+    const poolArray = [...pools, newPool];
+    setPools(poolArray);
+    console.log(pools);
+  }
+
+  
+
   return (
     <div>
       <Header/>
       <h1 className="text-3xl">
         Pool Page
       </h1>
+      <div>
+        <button onClick={makePool}>Create Pool</button>
+      </div>
+      <PoolList toBeListed = {pools}/>
     </div>
   )
 }
