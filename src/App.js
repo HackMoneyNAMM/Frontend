7ca0faa1de18c27796a8add645eb1f389dec126e
Evan W <ewitulsk@calpoly.edu>
2022-05-15 18:05:01 -0700

    Added chain opts, web3modal and wallet providers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import Header from "./header/Header";
 
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { useState } from "react";
+
 function App() {
+
+  const [provider, setProvider] = useState(null)
+  const [signer, setSigner] = useState(null)
+
+  const [activeChain, setActiveChain] = useState(null)
+
+  const supportedChainsInfo = {
+    polygon: {
+      mainnet: {
+        name: "Matic",
+        rpc: "https://polygon-rpc.com/",
+        chainId: 137,
+      },
+      testnet: {
+        name: "Matic Mumbai",
+        rpc: "https://rpc-mumbai.matic.today",
+        chainId: 80001,
+      }
+    },
+
+    arbitrum: {
+      mainnet: {
+        name: "Arbitrum One",
+        rpc: "https://arb1.arbitrum.io/rpc",
+        chainId: 42161
+      },
+      testnet: {
+        name: "Arbitrum Testnet",
+        rpc: "https://rinkeby.arbitrum.io/rpc",
+        chainId: 421611
+      }
+    },
+
+    //We'll revisit Oasis Later
+    //oasis: {
+      //mainnet: {},
+      //testnet: {}
+    //}
+  }
+
   return (
     <div>
-      <Header/>
+      <Header provider={provider} signer={signer} setProvider={setProvider} setSigner={setSigner} activeChain={activeChain} setActiveChain={setActiveChain}/>
       <h1 className="text-3xl font-bold underline">
         Welcome to our Nested AMM
       </h1>
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,52 @@
 import { Link } from "react-router-dom"
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+//import WalletConnectProvider from "@walletconnect/web3-provider";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 
-function Header() {
+
+function Header(props) {
+
+  async function connectWallet(){
+
+    const providerOpts = {
+    
+      /*
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          rpc: {
+            [props.activeChain.chainId]: props.activeChain.rpc
+          }
+        }
+      },*/
+  
+      coinbasewallet: {
+        package: CoinbaseWalletSDK,
+        options: {
+          appName: "HackMoney NAMM",
+          rpc: {
+            [props.activeChain.chainId]: props.activeChain.rpc
+          }
+        }
+      }
+  
+    }
+  
+    //const providerOpts = {}
+  
+    const web3Modal = new Web3Modal({
+      network: "testnet",
+      cacheProvider: true,
+      providerOpts
+  });
+
+    const instance = await web3Modal.connect()
+    const provider = new ethers.providers.Web3Provider(instance)
+    props.setProvider(provider)
+    props.setSigner(provider.getSigner())
+  }
+
   return (
     <nav className="grid grid-cols-5 gap 4">
       <div>
@@ -16,7 +62,7 @@ function Header() {
         <div>Select Chain</div>
       </div>
       <div>
-        <div>Connect Wallet</div>
+        <button onClick={connectWallet}>Connect Wallet</button>
       </div>
     </nav>
   )
