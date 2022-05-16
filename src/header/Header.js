df5597ca95bd8c513b70aebea8707ca9e4b5aed1
Evan W <ewitulsk@calpoly.edu>
2022-05-15 21:14:37 -0700

    Chain switch switches ish

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,11 +4,10 @@ import Web3Modal from "web3modal";
 //import WalletConnectProvider from "@walletconnect/web3-provider";
 import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 
-
 function Header(props) {
 
   async function connectWallet(){
-
+    console.log(props)
     const providerOpts = {
     
       /*
@@ -47,6 +46,27 @@ function Header(props) {
     props.setSigner(provider.getSigner())
   }
 
+  function setActiveChain(value, props) {
+    const chain = value
+    let activeChain = "None";
+    switch(chain){
+      case "Matic":
+        activeChain = props.supportedChainsInfo.polygon.mainnet
+        break
+      case "Matic Mumbai":
+        activeChain = props.supportedChainsInfo.polygon.testnet
+        break
+      case "Arbitrum One":
+        activeChain = props.supportedChainsInfo.arbitrum.mainnet
+        break
+      case "Arbitrum Testnet":
+        activeChain = props.supportedChainsInfo.arbitrum.testnet
+        break
+    }
+    console.log("Setting chain to: "+activeChain.name)
+    props.setActiveChain(activeChain)
+  }
+
   return (
     <nav className="grid grid-cols-5 gap 4">
       <div>
@@ -59,7 +79,17 @@ function Header(props) {
         <Link to="/poolPage">Pool</Link>
       </div>
       <div>
-        <div>Select Chain</div>
+        <label>Chain: </label>
+        <select name="chains" id="chains" onChange={(e) => setActiveChain(e.target.value, props)}>
+          <optgroup label="Mainnets">
+            <option value="Matic">Polygon</option>
+            <option value="Arbitrum One">Arbitrum</option>
+          </optgroup>
+          <optgroup label="Testnets">
+            <option value="Matic Mumbai">Mumbai</option>
+            <option value="Arbitrum Testnet">Arbitrum Testnet</option>
+          </optgroup>
+        </select>
       </div>
       <div>
         <button onClick={connectWallet}>Connect Wallet</button>
