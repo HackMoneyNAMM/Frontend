acc9e8d4364adc43479e15d1382a8407f16d639f
Trevor Bryant <tbryant.tbryant@gmail.com>
2022-05-21 22:42:28 -0700

    Testing graph queries

--- src/PoolPage/PoolPage.js
+++ src/PoolPage/PoolPage.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+const axios = require("axios");
+const endpoint = "https://api.thegraph.com/subgraphs/name/ewitulsk/hackmoneymumbai";
+const headers = {
+  "content-type": "application/json"
+};
 
 function PoolList(props) {
 
@@ -14,12 +19,32 @@ function PoolList(props) {
   );
 }
 
+async function getPools() {
+  const graphqlQuery = {
+    "operationName": "fetchPools",
+    "query": `query fetchPools { pools { id poolId poolAddr LPAddr totalTokenNum tokenAddresses name ticker reserves sigma eta} }`,
+    "variables": {}
+  };
+
+  const response = await axios({
+    url: endpoint,
+    method: 'post',
+    headers: headers,
+    data: graphqlQuery
+  });
+
+  console.log(response);
+  console.log(response.data);
+  console.log(response.errors);
+}
+
 function PoolPage(props) {
 
   const [pools, setPools] = useState([])
   
+
   useEffect(()=>{
-    
+    getPools();  
   }, [])
 
   return (
